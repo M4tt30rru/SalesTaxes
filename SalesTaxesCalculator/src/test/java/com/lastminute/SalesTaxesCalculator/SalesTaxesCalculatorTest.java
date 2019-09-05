91bb4ef6c8533af85f31dcf8d8af934f66ea0095
Matteo Orrù <matt.orru@gmail.com>
2019-09-05 19:45:47 +0200

    testing the first input

--- SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/SalesTaxesCalculatorTest.java
+++ SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/SalesTaxesCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.lastminute.SalesTaxesCalculator;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
@@ -14,10 +15,25 @@ public class SalesTaxesCalculatorTest {
 //		1 chocolate bar at 0.85
 
 	@Test
-	public void test() {
+	public void should_return_zero_taxes_for_books() {
 		SalesTaxesCalculator salesTaxesCalculator = new SalesTaxesCalculator();
 		Book book = new Book(12.49);
-		assertThat(salesTaxesCalculator.getTaxFromItem(book),equalTo(0.0));
+		assertThat(salesTaxesCalculator.getTaxFromItem(book), equalTo(0.0));
+	}
+	
+	
+	@Test
+	public void should_return_10_taxes_for_cd() {
+		SalesTaxesCalculator salesTaxesCalculator = new SalesTaxesCalculator();
+		CD cd = new CD(14.99);
+		assertThat(salesTaxesCalculator.getTaxFromItem(cd), equalTo(10.0));
+	}
+	
+	@Test
+	public void should_return_zero_taxes_for_chocholate_bar() {
+		SalesTaxesCalculator salesTaxesCalculator = new SalesTaxesCalculator();
+		ChocolateBar chocolate_bar = new ChocolateBar(0.85);
+		assertThat(salesTaxesCalculator.getTaxFromItem(chocolate_bar), equalTo(0.0));
 	}
 
 }
