e32f700e3f80be56238247bdb70a58e6d7d6fb75
Matteo Orrù <matt.orru@gmail.com>
2019-09-05 20:08:59 +0200

    green tests

--- SalesTaxesCalculator/src/main/java/com/lastminute/SalesTaxesCalculator/SalesTaxesCalculator.java
+++ SalesTaxesCalculator/src/main/java/com/lastminute/SalesTaxesCalculator/SalesTaxesCalculator.java
@@ -3,6 +3,8 @@ package com.lastminute.SalesTaxesCalculator;
 public class SalesTaxesCalculator {
 
 	public Double getTaxFromItem(Item item) {
+		if(item.getCategory() != "food" && item.getCategory() != "book" && item.getCategory() != "medicine")
+			return 10.0;
 		return 0.0;
 	}
 
--- SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/SalesTaxesCalculatorTest.java
+++ SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/SalesTaxesCalculatorTest.java
@@ -3,6 +3,7 @@ package com.lastminute.SalesTaxesCalculator;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SalesTaxesCalculatorTest {
@@ -13,26 +14,30 @@ public class SalesTaxesCalculatorTest {
 //		1 book at 12.49
 //		1 music CD at 14.99
 //		1 chocolate bar at 0.85
+	
+	private SalesTaxesCalculator salesTaxesCalculator;
+
+	@Before
+	public void setup() {
+		salesTaxesCalculator = new SalesTaxesCalculator();
+	}
 
 	@Test
 	public void should_return_zero_taxes_for_books() {
-		SalesTaxesCalculator salesTaxesCalculator = new SalesTaxesCalculator();
-		Book book = new Book(12.49);
+		Book book = new Book("book",12.49);
 		assertThat(salesTaxesCalculator.getTaxFromItem(book), equalTo(0.0));
 	}
 	
 	
 	@Test
 	public void should_return_10_taxes_for_cd() {
-		SalesTaxesCalculator salesTaxesCalculator = new SalesTaxesCalculator();
-		CD cd = new CD(14.99);
+		CD cd = new CD("cd",14.99);
 		assertThat(salesTaxesCalculator.getTaxFromItem(cd), equalTo(10.0));
 	}
 	
 	@Test
 	public void should_return_zero_taxes_for_chocholate_bar() {
-		SalesTaxesCalculator salesTaxesCalculator = new SalesTaxesCalculator();
-		ChocolateBar chocolate_bar = new ChocolateBar(0.85);
+		ChocolateBar chocolate_bar = new ChocolateBar("food",0.85);
 		assertThat(salesTaxesCalculator.getTaxFromItem(chocolate_bar), equalTo(0.0));
 	}
 
