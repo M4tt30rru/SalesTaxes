356486ba3ae5914c949dc1e7b78b66d09a7d6986
Matteo Orrù <matt.orru@gmail.com>
2019-09-05 22:09:09 +0200

    imported book

--- SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/Book.java
+++ SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/Book.java
@@ -3,6 +3,7 @@ package com.lastminute.SalesTaxesCalculator;
 public class Book extends Item {
 	
 	private static final double TAX = 0;
+	private boolean imported;
 
 
 	public Book(String name, double price) {
@@ -11,8 +12,13 @@ public class Book extends Item {
 
 	}
 
+	public Book(String name, double price, boolean imported) {
+		this(name, price);
+		this.imported = imported;
+	}
+
 	public double getPrice() {
-		return this.price;
+		return this.price + (imported ? this.price * 0.05 : 0);
 	}
 
 	public double getTax() {
