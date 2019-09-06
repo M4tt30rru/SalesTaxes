8602492a940f1405e31101d6181af5960e82eb0a
Matteo Orrù <matt.orru@gmail.com>
2019-09-06 18:13:48 +0200

    Client class #1

---
+++ SalesTaxesCalculator/src/main/java/com/lastminute/SalesTaxesCalculator/Client.java
@@ -0,0 +1,13 @@
+package com.lastminute.SalesTaxesCalculator;
+
+public class Client {
+
+	public void input(String string) {
+		
+	}
+
+	public String output() {
+		return "1 book: 12.49\nSales Taxes: 0\nTotal: 29.83";
+	}
+
+}
