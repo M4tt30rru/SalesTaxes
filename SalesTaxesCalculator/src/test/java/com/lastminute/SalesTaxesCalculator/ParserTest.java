638d531f5b761da13ce2fe4752314b02ccafd7e6
Matteo Orrù <matt.orru@gmail.com>
2019-09-07 10:45:07 +0200

    mocking the abstract factory class for items

--- SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/ParserTest.java
+++ SalesTaxesCalculator/src/test/java/com/lastminute/SalesTaxesCalculator/ParserTest.java
@@ -8,18 +8,23 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
 
 public class ParserTest {
 	
+	// @Mock
+	private ISalesTaxesCalculator salesTaxesCalculator;
+	
 	@Mock
-	private ISalesCalculator salesCalculator;
+	private AbstractItemFactory itemFactory;
 	
 	@InjectMocks
 	private IParser parser;
 
 	@Before
 	public void setup() {
-		parser = new Parser(salesCalculator);
+		salesTaxesCalculator = new SalesTaxesCalculator();
+		parser = new Parser(salesTaxesCalculator,itemFactory);
 		MockitoAnnotations.initMocks(this);
 	}
 
@@ -31,8 +36,17 @@ public class ParserTest {
 	
 	@Test
 	public void should_return_complete_output_for_1_cd() {
+		// when(salesTaxesCalculator.getTaxFromItem("1 music CD at 14.99")).thenReturn(1.50);
+		when(itemFactory.createItem(1,"music CD",14.99)).thenReturn(new Other("music CD",14.99,false));
 		String parsed = parser.parse("1 music CD at 14.99");
-		assertThat(parsed,equalTo("1 music CD: 16.49\nSales Taxes: 1.50\nTotal: 17.99"));
+		assertThat(parsed,equalTo("1 music CD: 16.49\nSales Taxes: 1.50\nTotal: 16.49"));
+		verify(itemFactory, times(1)).createItem(1, "music CD",14.99);
+	}
+	
+	@Test
+	public void should_return_item_name() {
+		String parsed = parser.getItemName("1 music CD at 14.99");
+		assertThat(parsed,equalTo("music CD"));
 	}
 
 }
