d51593e06b9109fbcff41ce014a42328ef0ee44a
Matteo Orrù <matt.orru@gmail.com>
2019-09-08 14:14:58 +0200

    Client done (close #1 close #8)

--- SalesTaxesCalculator/src/main/java/com/lastminute/SalesTaxesCalculator/Client.java
+++ SalesTaxesCalculator/src/main/java/com/lastminute/SalesTaxesCalculator/Client.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.lastminute.SalesTaxesCalculator.factory.ItemFactory;
+
 public class Client {
 
 	private IParser parser;
